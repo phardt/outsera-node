70dfff681bfc5e16258ce6d25c861ef722e4df90
Phyllip Hardt Silva <phyllip.hs@gmail.com>
2024-12-20 14:16:07 -0300

    fix: adjust logit to search all producers

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -3,13 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Movie } from './models/movie.schema';
 import { Model } from 'mongoose';
 import { WinnerDto, WinnerResponseDto } from './dtos/winner.dto';
-import { ListHelper } from './commons/list.helper';
 
 type MovieGroup = {
   name: string;
   interval: number;
   movies: Movie[];
 };
+
+type valueType = 'min' | 'max';
 
 @Injectable()
 export class MoviesService {
@@ -21,46 +22,60 @@ export class MoviesService {
       .sort({ year: 1 })
       .exec();
 
-    const duplicattedMovies = ListHelper.getAllDuplicateItems(
-      movies,
-      'producers',
+    const groups = this.groupMoviesByProducer(movies).filter(
+      (item) => item.movies.length > 1,
     );
 
-    const max = this.getBigestYearInterval(duplicattedMovies);
-    const min = max[0]
-      ? this.getSmallYearInterval(duplicattedMovies, max[0].interval)
-      : [];
+    const max = this.getYearInterval(groups, 'max', 0);
+    const min =
+      max.length > 0
+        ? this.getYearInterval(groups, 'min', max[0].interval)
+        : [];
     const winnerDtoRespoonse: WinnerResponseDto = { min, max };
     return winnerDtoRespoonse;
   }
 
-  groupMoviesByProducer(movies: Movie[]): MovieGroup[] {
-    const group: MovieGroup[] = [];
+  private groupMoviesByProducer(movies: Movie[]): MovieGroup[] {
+    const groups: Record<string, MovieGroup> = {};
 
-    movies.forEach((item) => {
-      const producers = movies.filter(
-        (mItem) => mItem.producers === item.producers,
-      );
+    movies.forEach((movie) => {
+      const producers = movie.producers
+        .split(/,\s*|\sand\s/g)
+        .map((name) => name.trim());
 
-      const groupProducer = group.find(
-        (gItem) => gItem.name === item.producers,
-      );
+      producers.forEach((producer) => {
+        if (!groups[producer]) {
+          groups[producer] = { name: producer, movies: [], interval: 0 };
+        }
 
-      if (groupProducer == null) {
-        const interval =
-          producers[producers.length - 1].year - producers[0].year;
-        group.push({ name: item.producers, movies: producers, interval });
-      }
+        const group = groups[producer];
+        group.movies.push(movie);
+        group.interval = movie.year - group.movies[0].year;
+      });
     });
-    return group;
+
+    return Object.values(groups);
   }
 
-  getBigestYearInterval(movies: Movie[]): WinnerDto[] {
-    let lstMovies = [];
-    const group = this.groupMoviesByProducer(movies);
+  private compareTypeValue(
+    typeValue: valueType,
+    initialInterval: number,
+    interval: number,
+  ) {
+    return typeValue === 'max'
+      ? initialInterval > interval
+      : initialInterval < interval;
+  }
 
-    group.reduce((prev, curr) => {
-      if (curr.interval > prev) {
+  private getYearInterval(
+    groups: MovieGroup[],
+    typeValue: valueType,
+    initialValue,
+  ): WinnerDto[] {
+    let lstMovies = [];
+
+    groups.reduce((prev, curr) => {
+      if (this.compareTypeValue(typeValue, curr.interval, initialValue)) {
         lstMovies = [curr];
         return curr.interval;
       } else if (curr.interval === prev) {
@@ -69,41 +84,21 @@ export class MoviesService {
       }
 
       return prev;
-    }, 0);
+    }, initialValue);
 
     return this.makeWinnerDto(lstMovies);
   }
 
-  getSmallYearInterval(movies: Movie[], intervalInit: number): WinnerDto[] {
-    let lstMovies = [];
-    const group = this.groupMoviesByProducer(movies);
-
-    group.reduce((prev, curr) => {
-      if (curr.interval < prev) {
-        lstMovies = [curr];
-        return curr.interval;
-      } else if (curr.interval === prev) {
-        lstMovies.push(curr);
-        return curr.interval;
-      }
-
-      return prev;
-    }, intervalInit);
-
-    return this.makeWinnerDto(lstMovies);
-  }
-
-  makeWinnerDto(lstGroup: MovieGroup[]): WinnerDto[] {
+  private makeWinnerDto(lstGroup: MovieGroup[]): WinnerDto[] {
     const lstWinner: WinnerDto[] = lstGroup.map((item) => {
       const followingWin = item.movies[item.movies.length - 1].year;
       const previousWin = item.movies[0].year;
-      const winner: WinnerDto = {
+      return {
         producer: item.name,
         followingWin,
         previousWin,
         interval: item.interval,
       };
-      return winner;
     });
     return lstWinner;
   }
